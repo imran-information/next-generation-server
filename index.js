d6266b40cb751215c971b9a85bf30edd26338103
imranhossen21 <imranhossen0586@gmail.com>
2024-12-26 22:20:24 +0600

    all done

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ async function run() {
 
 
 
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
